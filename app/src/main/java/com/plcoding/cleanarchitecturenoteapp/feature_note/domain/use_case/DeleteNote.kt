24053913bf499dd81938c0333cd5ca20b5cdf910
elalejr <edward.gratefuldead1@gmail.com>
2022-12-22 18:05:25 -0600

    comment

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNote.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNote.kt
@@ -3,6 +3,8 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
 
+
+// use case for deleting a note
 class DeleteNote(
     private val repository: NoteRepository
 ) {
