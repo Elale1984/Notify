40588e84440336e50e474094a0c11f12b336a41f
elalejr <edward.gratefuldead1@gmail.com>
2022-12-22 18:01:35 -0600

    usecases get all and delete

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -3,12 +3,14 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.plcoding.cleanarchitecturenoteapp.ui.theme.*
+import java.sql.Timestamp
 
 @Entity
 data class Note(
     val title: String,
     val content: String,
-    val color: Long,
+    val timestamp: Long,
+    val color: Int,
     @PrimaryKey val id: Int? = null
 ) {
     companion object {
